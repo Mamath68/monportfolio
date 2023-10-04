b79992694099df98765695e8129815d2f7356ccd
Mamath68 <mathieu.stamm@gmail.Com>
2023-10-04 13:09:10 +0200

    modif taille hauteur

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,13 +4,13 @@ const About = () => {
     return (
         <div
             name="about"
-            className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+            className="w-full min-min-h-screen bg-gradient-to-b from-gray-800 to-black text-white"
         >
-            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-screen">
+            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full min-min-h-screen">
                 <div className="pb-8">
                     <p className="text-4xl font-bold inline border-b-4 border-gray-500">
                         About
-                        </p>
+                    </p>
                 </div>
 
                 <p className="text-xl mt-20 ">
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -54,33 +54,33 @@ const Contact = () => {
     // ]
 
     return (
-        <div 
-        name="contact" 
-        className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white">
-            <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-screen ">
+        <div
+            name="contact"
+            className="w-full min-h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white">
+            <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto min-h-screen ">
                 <div className="pb-8">
                     <p className="text-4xl font-bold inline border-b-4 border-gray-500">Contact</p>
                     <p className="py-6">Subtmit the form below to get in touch with me</p>
 
                     <div className="flex justify-center items-center">
                         <form action="https://getform.io/f/a8940881-04f5-47e6-93bf-bfde0fc31015" method="post" className="flex flex-col w-full md:w-1/2  ">
-                            <input 
-                            type="text" 
-                            name="name" 
-                            placeholder='Enter Your name' 
-                            className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none " 
+                            <input
+                                type="text"
+                                name="name"
+                                placeholder='Enter Your name'
+                                className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none "
                             />
-                            <input 
-                            type="email" 
-                            name="email" 
-                            placeholder='Enter Your email' 
-                            className=" my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none " 
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder='Enter Your email'
+                                className=" my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none "
                             />
-                            <textarea 
-                            name="message" 
-                            rows="10"
-                            placeholder='Enter Your Message' 
-                            className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none">
+                            <textarea
+                                name="message"
+                                rows="10"
+                                placeholder='Enter Your Message'
+                                className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none">
                             </textarea>
 
                             <button type="submit" className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">Let's Talk</button>
